b66cdba37a3e4a8ea5b2336533c199a6ff2dc5c6
alanclebersantana <alanclebersantana@gmail.com>
2026-09-18 17:46:43 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
    Estratégia: rede primeiro, cache como rede de segurança.
    O sistema é online (Firestore), então nunca servimos dado velho quando há conexão;
    o cache existe para a casca do app abrir offline e para a instalação funcionar. */
-const VERSAO = 'mp-2026-09-18-01';
-const CASCA = ['./', './index.html', './instalar.html', './manifest.webmanifest',
+const VERSAO = 'mp-2026-09-18-02';
+const CASCA = ['./', './index.html', './instalar.html', './mobile.html', './manifest.webmanifest',
                './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
 self.addEventListener('install', e => {
